dc53458482add775004ece190bc788fd2f9620dc
上出　和馬 <uede@d-in.jp>
2019-03-14 17:32:02 +0900

    トップへ行くボタンの表示、非表示

--- js/jquery1.js
+++ js/jquery1.js
@@ -4,16 +4,21 @@ $(function(){
     $(document).on('click','.to_top',function(){
         $('html, body').animate({scrollTop: 0},500,'swing');
     });
-    // トップボタンをフェードイン
-    $(window).scroll(function() {
-        $('.to_top').fadeTo("slow",1);
+    // トップボタンを表示、非表示
+    $(window).scroll(function () {
+        if($(window).scrollTop() >= 200) {
+            $('.to_top').animate({ width: 'show'}, 'fast' );
+        } else {
+            $('.to_top').animate({ width: 'hide'}, 'fast' );
+        }
     });
-
     // 次のセレクションへ移動
     $(document).on('click','.nextsection',function(){
         var index = $(".nextsection").index(this) + 1 ;
         $('html, body').animate({scrollTop:$(".fill_section").eq(index).offset().top},500,'swing')
     });
+
+
 
 
 
