1c1f3e9c3d43b8b39bb8a20d9fda70d05962581b
上出　和馬 <uede@d-in.jp>
2019-03-15 10:40:48 +0900

    ページのスクロール表示機能

--- js/jquery1.js
+++ js/jquery1.js
@@ -1,5 +1,13 @@
 $(function(){
     $('.to_top').hide();
+    var i = 1;
+    var section_top = [];
+
+    while (i< 4){
+        section_top[i] = $(".fill_section").eq(i).offset().top;
+        $(".fill_section").eq(i).animate({ height: 'hide'});
+        i++;
+    }
     // トップに戻る
     $(document).on('click','.to_top',function(){
         $('html, body').animate({scrollTop: 0},500,'swing');
@@ -11,15 +19,17 @@ $(function(){
         } else {
             $('.to_top').animate({ width: 'hide'}, 'fast' );
         }
+        var i = 1;
+        while(i< 4){
+            if($(window).scrollTop() >= section_top[i] - 500 ){
+                $(".fill_section").eq(i).animate({ height: 'show'}, 'fast' );
+            }
+            i++;
+        }
     });
     // 次のセレクションへ移動
     $(document).on('click','.nextsection',function(){
         var index = $(".nextsection").index(this) + 1 ;
         $('html, body').animate({scrollTop:$(".fill_section").eq(index).offset().top},500,'swing')
     });
-
-
-
-
-
 });
